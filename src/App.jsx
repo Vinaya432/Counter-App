bc0db815a379cc717a116b66d39f3393253728d7
Vinaya432 <vinayam432@gmail.com>
2023-12-27 12:18:35 +0530

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './components/Counter'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+    <div style={{height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center'>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
